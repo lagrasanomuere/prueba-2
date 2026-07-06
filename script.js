46aa6782fd92cdb01e4c9bbfb20cdd25fb886960
lagrasanomuere <bnett106@gmail.com>
2026-07-05 22:25:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+const mensajes = document.getElementById("mensajes");
 
+const input = document.getElementById("mensaje");
+
+const boton = document.getElementById("btnEnviar");
+
+function enviar(){
+
+    if(input.value.trim()=="") return;
+
+    mensajes.innerHTML += `
+        <div class="msg">
+            <span class="user">Tú:</span>
+            ${input.value}
+        </div>
+    `;
+
+    mensajes.scrollTop = mensajes.scrollHeight;
+
+    input.value="";
+}
+
+boton.onclick = enviar;
+
+input.addEventListener("keypress",function(e){
+
+    if(e.key==="Enter"){
+
+        enviar();
+
+    }
+
+});
